a4f2ce0d834aa3a2a3b4f129aa8db22cd9e537d4
Tomokazuha <1335197400@qq.com>
2025-04-15 16:33:31 +0800

    Delete run_test.bat

--- run_test.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd app\modules\question_generator
-set PYTHONPATH=%PYTHONPATH%;%CD%\..\..\..
-python generator_test.py --dimensions "医学,法学" --difficulties "easy,medium,hard" --distribution "balanced" --count 10 --output "test_questions.json"
-pause 
